98a216b2308c34af23988554f6899e5f7d83d3fb
likeablob <46628917+likeablob@users.noreply.github.com>
2019-10-12 01:28:34 +0900

    Specify the depth explicitly - The depth mismatch may cause blank screen

--- switcher/sh.d/1_large_minimac.sh
+++ switcher/sh.d/1_large_minimac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 vncserver -kill :1
-vncserver -geometry 512x384
+vncserver -geometry 512x384 -depth 24
 ssvncviewer -scale 0.625 :1 -fullscreen -passwd ~/.vnc/passwd &
 DISPLAY=:1
 cd ~/macmini/minivmac
--- switcher/sh.d/2_minimac.sh
+++ switcher/sh.d/2_minimac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 vncserver -kill :1
-vncserver -geometry 320x240
+vncserver -geometry 320x240 -depth 24
 ssvncviewer -scale 1 :1 -fullscreen -passwd ~/.vnc/passwd &
 DISPLAY=:1
 cd ~/macmini/minivmac
